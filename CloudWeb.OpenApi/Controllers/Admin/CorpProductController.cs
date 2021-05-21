429d859784164d85a5c1b4a3202a8e35e5b1d538
Haibaotao <879264743@qq.com>
2021-05-21 14:25:16 +0800

    后台接口

---
+++ CloudWeb.OpenApi/Controllers/Admin/CorpProductController.cs
@@ -0,0 +1,82 @@
+﻿using CloudWeb.Dto;
+using CloudWeb.Dto.Common;
+using CloudWeb.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+
+namespace CloudWeb.OpenApi.Controllers.Admin
+{
+    /// <summary>
+    /// 公司产品相关的接口
+    /// </summary>
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class CorpProductController : Controller
+    {
+        private readonly ILogger<CorpProductsDto> _logger;
+        private readonly ICorpProductsService _service;
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>      
+        public CorpProductController(
+            ILogger<CorpProductsDto> logger,
+            ICorpProductsService service)
+        {
+            _logger = logger;
+            _service = service;
+        }
+
+        /// <summary>
+        /// 查询所有未删除的产品
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ResponseResult<IEnumerable<CorpProductsDto>> GetAll()
+        {
+            return _service.GetProducts();
+        }
+
+        /// <summary>
+        /// 获取产品信息
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ResponseResult<CorpProductsDto> GetProductById(int id)
+        {
+            return _service.GetProductsById(id);
+        }
+
+        /// <summary>
+        /// 获取公司产品
+        /// </summary>
+        /// <param name="corpId"></param>
+        /// <returns></returns>
+        public ResponseResult<IEnumerable<CorpProductsDto>> GetProductByCorp(int corpId)
+        {
+            return _service.GetProductsByCorpId(corpId);
+        }
+
+        /// <summary>
+        /// 添加产品
+        /// </summary>
+        /// <param name="corpProduct"></param>
+        /// <returns></returns>
+        public ResponseResult<bool> addProduct(CorpProductsDto corpProduct)
+        {
+            return _service.AddProduct(corpProduct);
+        }
+
+        /// <summary>
+        /// 删除产品
+        /// </summary>
+        /// <param name="ids"></param>
+        /// <returns></returns>
+        public ResponseResult<bool> DelProduct(dynamic[] ids)
+        {
+            return _service.DelProduct(ids);
+        }
+    }
+}
--- CloudWeb.OpenApi/Controllers/Admin/CorporationController.cs
+++ CloudWeb.OpenApi/Controllers/Admin/CorporationController.cs
@@ -3,12 +3,12 @@ using CloudWeb.Dto.Common;
 using CloudWeb.IServices;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-
+using System.Collections.Generic;
 
 namespace CloudWeb.OpenApi.Controllers.Admin
 {
     /// <summary>
-    /// 公司相关Api
+    /// 公司相关接口
     /// </summary>
     [Produces("application/json")]
     [Route("api/[controller]")]
@@ -24,7 +24,60 @@ namespace CloudWeb.OpenApi.Controllers.Admin
             ILogger<CorporationDto> logger,
             ICorporationService corporationService)
         {
-
+            _logger = logger;
+            _corporationService = corporationService;
         }
+
+        /// <summary>
+        /// 查询所有未删除的公司信息
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ResponseResult<IEnumerable<CorporationDto>> GetAll()
+        {
+            return _corporationService.GetAllCorporation();
+        }
+
+        /// <summary>
+        /// 查询公司信息
+        /// </summary>
+        /// <param name="id">公司id</param>
+        /// <returns></returns>
+        public ResponseResult<CorporationDto> GetCorporation(int id)
+        {
+            return _corporationService.GetCorporation(id);
+        }
+
+        /// <summary>
+        /// 修改公司信息
+        /// </summary>
+        /// <returns></returns>
+        [HttpPut]
+        public ResponseResult<bool> UpdateCorporation(CorporationDto corporation)
+        {
+            return _corporationService.UpdateCorporation(corporation);
+        }
+
+        /// <summary>
+        /// 添加公司信息
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public ResponseResult<bool> addCorporaion(CorporationDto corporation)
+        {
+
+            return _corporationService.AddCorporation(corporation);
+        }
+
+        /// <summary>
+        /// 删除公司信息
+        /// </summary>
+        /// <param name="ids"></param>
+        /// <returns></returns>
+        public ResponseResult<bool> DeleteColumn(dynamic[] ids)
+        {
+            return _corporationService.DelCorporation(ids);
+        }
+
     }
 }
