b661aaf13bdb33a7986b306a303ca71063c506b1
Haibaotao <879264743@qq.com>
2021-05-22 13:53:05 +0800

    添加

---
+++ CloudWeb.Web/Controllers/CorporationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CloudWeb.Web.Controllers
+{
+    public class CorporationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
