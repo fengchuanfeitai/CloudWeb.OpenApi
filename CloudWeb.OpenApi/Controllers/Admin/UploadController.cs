28af3dc66a4efe1fb540cc3972516b48ac77a318
fengchuan Chen <1073690406@qq.com>
2021-05-23 14:56:28 +0800

    上传方法，微测试

--- CloudWeb.OpenApi/Controllers/Admin/UploadController.cs
+++ CloudWeb.OpenApi/Controllers/Admin/UploadController.cs
@@ -1,67 +1,77 @@
 ﻿using CloudWeb.Dto.Common;
 using CloudWeb.Util;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CloudWeb.OpenApi.Controllers.Admin
 {
+    /// <summary>
+    /// 上传文件
+    /// </summary>
     public class UploadController : Controller
     {
-        [Produces("application/json")]
-        [Route("api/admin/[controller]/[action]")]
+        private readonly ILogger<UploadController> _logger;
+
+        public UploadController(ILogger<UploadController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// 上传文件
+        /// </summary>
+        /// <returns></returns>
         [HttpPost]
-        public ResponseResult<string> UploadImg(string file)
+        [Produces("application/json")]
+        [Route("api/admin/upload")]
+        public ResponseResult<string> UploadImg()
         {
             ResponseResult<string> responseResult = new ResponseResult<string>();
             try
             {
-                FileInfo fi = new FileInfo(file);
-                string ext = fi.Extension;
-                var orgFileName = fi.Name;
-                var newFileName = Guid.NewGuid() + ext;
-                var filePath = Path.Combine("", newFileName);
-                using (var stream = new FileStream(filePath, FileMode.Create))
+                var files = Request.Form.Files;
+
+                if (files != null && files.Count > 0)
                 {
-                     //formFile.CopyToAsync(stream);
+                    var file = files[0];
+                    string getex = file.FileName.Substring(file.FileName.LastIndexOf('.')).ToLower();
+
+                    if (FileUpLoad.IsImgFile(getex))
+                    {
+                        string path = string.Concat(AppContext.BaseDirectory, "~/Upload/");
+                        string newName = FileUpLoad.CreateFileName(getex);
+
+                        if (!Directory.Exists(path))
+                        {
+                            Directory.CreateDirectory(path);
+                        }
+
+                        using (var stream = new FileStream(path, FileMode.Create))
+                        {
+                            file.CopyTo(stream);
+                            path = path + newName;
+                        }
+
+                        responseResult.code = 0;
+                        responseResult.data = "/Upload/" + newName;
+                    }
+                    else
+                    {
+                        responseResult.code = -1;
+                        responseResult.msg = "不是有效的文件";
+                    }
                 }
-
-
-                //if (files != null && files.Count > 0)
-                //{
-                //    var file = files[0];
-                //    string getex = file.FileName.Substring(file.FileName.LastIndexOf('.')).ToLower();
-                //    if (FileUpLoad.IsImgFile(getex))
-                //    {
-                //        string path = Server.MapPath("~/Upload/");
-                //        string newName = FileUpLoad.CreateFileName(getex);
-                //        if (!Directory.Exists(path))
-                //        {
-                //            Directory.CreateDirectory(path);
-                //        }
-                //        path = path + newName;
-                //        file.SaveAs(path);
-
-                //        responseResult.code = 0;
-                //        responseResult.data = "/Upload/" + newName;
-                //    }
-                //    else
-                //    {
-                //        responseResult.code = -1;
-                //        responseResult.msg = "不是有效的文件";
-                //    }
-
-                //}
             }
             catch (Exception e)
             {
+                _logger.LogError($"文件上传失败：{e.Message}");
                 responseResult.code = -1;
                 responseResult.msg = "上传失败！";
             }
             return responseResult;
         }
+
     }
 }
--- CloudWeb.Web/Controllers/ColumnController.cs
+++ CloudWeb.Web/Controllers/ColumnController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CloudWeb.Util;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -18,5 +20,59 @@ namespace CloudWeb.Web.Controllers
         {
             return View();
         }
+
+        public JsonResult UploadImg()
+        {
+            ResponseResult responseResult = new ResponseResult();
+            try
+            {
+                var files = Request.Form.Files;
+
+                if (files != null && files.Count > 0)
+                {
+                    var file = files[0];
+                    string getex = file.FileName.Substring(file.FileName.LastIndexOf('.')).ToLower();
+                    if (FileUpLoad.IsImgFile(getex))
+                    {
+                        string path = Server.MapPath("~/Upload/");
+                        string newName = FileUpLoad.CreateFileName(getex);
+                        if (!Directory.Exists(path))
+                        {
+                            Directory.CreateDirectory(path);
+                        }
+
+
+                        using (var stream = new FileStream(path, FileMode.Create))
+                        {
+
+                            //图片路径保存到数据库里面去
+                            //bool flage = QcnApplyDetm.FinancialCreditQcnApplyDetmAdd(EntId, CrtUser, new_path);
+                            //if (flage == true)
+                            //{
+                            //再把文件保存的文件夹中
+                            file.CopyTo(stream);
+                            path = path + newName;
+
+                        }
+
+                        responseResult.ResultFlag = 0;
+                        responseResult.ResultData = "/Upload/" + newName;
+                    }
+                    else
+                    {
+                        responseResult.ResultFlag = -1;
+                        responseResult.ResultMessage = "不是有效的文件";
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                responseResult.ResultFlag = -1;
+                responseResult.ResultMessage = "上传失败！";
+            }
+            return Json(responseResult, JsonRequestBehavior.AllowGet);
+        }
+
     }
 }
