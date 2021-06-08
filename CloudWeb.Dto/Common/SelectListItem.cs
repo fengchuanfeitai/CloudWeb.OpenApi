0e38aba35fa5cfadcd1b0ed502b1969b9e18e2a2
Haibaotao <879264743@qq.com>
2021-06-08 17:39:19 +0800

    修改

---
+++ CloudWeb.Dto/Common/SelectListItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CloudWeb.Dto.Common
+{
+    public class SelectListItem
+    {
+        /// <summary>
+        /// 值
+        /// </summary>
+        public int Value { get; set; }
+
+        /// <summary>
+        /// 文本
+        /// </summary>
+        public string Text { get; set; }
+    }
+}
